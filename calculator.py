8fd268de10445fbcd05c195ebb8467305ffa2184
KKingSabs <p243006@pwr.nu.edu.pk>
2026-09-17 10:37:53 +0500

    premium-member-feature

--- calculator.py
+++ calculator.py
@@ -1,2 +1,8 @@
-def calculate_discount(price):
+def calculate_discount(price,membertype):
+    if membertype == "regular":
+        return price *0.2
+
+    if membertype == "premium":
+        return price *0.3        
+
     return price * 0.10
